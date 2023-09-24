845dea79ffed271018eaac1be73957c040493c20
SHINOHARA <020806zhaoyang@gmail.com>
2023-09-25 06:07:33 +0900

    yes

--- MySpotify/main.py
+++ MySpotify/main.py
@@ -3,12 +3,8 @@ import os
 
 from spo import MySpotify, a_scope
 
-load_dotenv()
-
-id = os.getenv("SPOTIPY_CLIENT_ID")
-
-print(id)
+name = input("Enter the name of an Artist: ")
 
 sp = MySpotify(0, a_scope)
-print("Artist: " + sp.get_artist("A$AP")["name"] + "\n"
-      "ID: " + sp.get_artist("A$AP")["id"] + "\n")
+print("Artist: " + sp.get_artist(name)["name"] + "\n"
+      "ID: " + sp.get_artist(name)["id"] + "\n")
